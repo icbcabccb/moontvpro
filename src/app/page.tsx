b7f41b694ae1043047c9a54cb3cfd914ef9195bb
Kevin T Garland <usd872@outlook.com>
2026-08-01 12:17:20 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1080,11 +1080,11 @@ function HomeClient() {
           <img 
             src="/logo.png" 
             alt="红月搜索 Logo" 
-            className="w-6 h-6 object-contain" 
+            className="w-12 h-12 object-contain" 
           />
           
           {/* SVG Logo */}
-          <svg className="w-6 h-6 text-red-500" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+          <svg className="w-12 h-12 text-red-500" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
 
